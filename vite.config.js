2a9f79ea966ef93ef3013143a9a3b19765c0cb13
Aum <parmaraum31@gmail.com>
2026-07-07 21:27:35 +0530

    Updates for today

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+<<<<<<< HEAD
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+=======
+import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve:{
+    alias:{
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
+})
+
+>>>>>>> bf98eae (Updates for today)
